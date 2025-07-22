b9d2cf730e7b458e661a70c940afd95ba69ddd03
Arden <121777799+ardenBarden@users.noreply.github.com>
2025-07-22 00:30:04 +0000

    Added relevant views and urls, created initial home page, added css skeleton

--- artwebsite/ardenapp/views.py
+++ artwebsite/ardenapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Display a simple welcome page where users can navigate to various pages. 
+# Initially, let's start with an about page.
+def home(request):
+    return render(request, 'ardenapp/home.html')
+
+
+def members(request):
+    return HttpResponse("Hello world!")

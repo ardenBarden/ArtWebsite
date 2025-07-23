a99ef358728707f3bf2db21c7b9fee9674208622
Arden <121777799+ardenBarden@users.noreply.github.com>
2025-07-23 01:02:41 +0000

    made it so that the text doesnt retype if you revisit a page. Need to make the button appear in good timing on namegiven if the text isnt spencer

--- artwebsite/ardenapp/urls.py
+++ artwebsite/ardenapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('/cordial-invite', views.invite, name='cordial-invite'),
+    path('/agenda', views.agenda, name='agenda'),
+    path('/homeagain', views.homeagain, name='homeagain'),
 ]
--- artwebsite/ardenapp/views.py
+++ artwebsite/ardenapp/views.py
@@ -5,14 +5,27 @@ from django.http import HttpResponse
 # Initially, let's start with an about page.
 def home(request):
     if request.method == 'POST':
-        print("ok")
+        #trying to make it so that revisiting a page doesnt replay animations
         name = {
             'name': request.POST.get('name'),
         } 
-        print(name['name']) # ok so it's there...
         return render(request, 'ardenapp/namegiven.html', name)
     else: 
         return render(request, 'ardenapp/home.html')
+    
+#for when he's revisiting and doesn't need the animations 
+def homeagain(request):
+    if request.method == 'POST':
+        #trying to make it so that revisiting a page doesnt replay animations
+        name = {
+            'name': request.POST.get('name'),
+        } 
+        return render(request, 'ardenapp/namegiven.html', name)
+    else: 
+        return render(request, 'ardenapp/homeagain.html')
 
 def invite(request):
     return render(request, 'ardenapp/cordialinvite.html')
+
+def agenda(request):
+    return render(request, 'ardenapp/agenda.html')
